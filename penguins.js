fde6555ebd28389bd69f7ef7d9710c350da9e36f
Malachi I. Beerram <malachi.beerram@centre.edu>
2019-10-24 12:04:44 -0400

    10/24ApFinal

--- penguins.js
+++ penguins.js
@@ -1,6 +1,6 @@
 var mainCode = function(penguinsData){
     //Draws the home data
-    var drawhome = function(){
+    var drawhome = function(penguinsDquizMean){
          d3.selectAll("body *").remove()
             d3.select("body").append("div").attr("id","heading").attr("class","page_asset").text("CLASSROOM")
             d3.select("body").append("div").attr("id","banner").attr("class","page_asset").text("Student INFO")
@@ -164,6 +164,8 @@ var mainCode = function(penguinsData){
 
         var i;
         var penguinsDMain = []
+        //(final 35%, Tests 30%, Quizes 20%, and HW is 15%)
+        //creats objects
         for (i = 0; i < penguinsData.length; i++) 
         {
             var penguins = {}
@@ -172,17 +174,25 @@ var mainCode = function(penguinsData){
             penguins.homework_mean = arrayofHW[i]
             penguins.test_mean = arrayofTest[i]
             penguins.final_test = arrayofFinal[i]
+            penguins.final_grade =(penguins.homework_mean*.15)+(penguins.quiz_mean*.20)+(penguins.test_mean*.30)+(penguins.final_test*.35) 
+            
+            penguins.final_grade_color = "green";
+            if (penguins.final_grade < 80) {penguins.final_grade_color = "#f39c12";}
+            if (penguins.final_grade < 70) {penguins.final_grade_color = "#e74c3c";}
+            if (penguins.final_grade < 60) {penguins.final_grade_color = "#c0392b";}
+            if (penguins.final_grade < 50) {penguins.final_grade_color = "#EA2027";}
+            
             penguinsDMain.push(penguins)
         }
 
-        //(final 35%, Tests 30%, Quizes 20%, and HW is 15%)
+        
 
         console.log("classData 2: ",penguinsDMain)
         
         return penguinsDMain
     }
 
-    var drawdata = function(penguinsDMain)
+    var drawdata = function(penguinsDataMain)
     {
         //Sudo function for drawing data for pictures 
         var drawHeading = function(){
@@ -217,11 +227,17 @@ var mainCode = function(penguinsData){
                 .attr("class","L_main_table_head")
                 .attr("id","L_base_table_final_test")
                 .text("final_test")
+
+            d3.select("#L_base_table")
+                .append("th")
+                .attr("class","L_main_table_head")
+                .attr("id","L_base_table_final_grade")
+                .text("final_grade")
         }
             drawHeading()
         
         //Draws ID photo based on location in main array
-        var drawIDS = function(penguinsDMain){
+        var drawIDS = function(penguinsDataMain){
         d3.select("#L_base_table")
             .selectAll("tr")
             .data(penguinsDMain)
@@ -237,7 +253,7 @@ var mainCode = function(penguinsData){
            drawIDS(penguinsDMain)
         
         //Draws intel based on ID photo location
-        var drawIntelforID = function(penguinsDMain){
+        var drawIntelforID = function(penguinsDataMain){
             penguinsDMain.forEach(function(aPenguins){
                 d3.select("#"+aPenguins.picture.substring(0, aPenguins.picture.length - 4))
                     .append("td")
@@ -253,37 +269,129 @@ var mainCode = function(penguinsData){
                     .attr("class","quiz_mean")
                     .text(function(penguinsDMain){return penguinsDMain.quiz_mean}) 
 
-               d3.select("#"+aPenguins.picture.substring(0, aPenguins.picture.length - 4))
+                d3.select("#"+aPenguins.picture.substring(0, aPenguins.picture.length - 4))
                     .append("td")
                     .attr("class","main_table_Cols")
                     .append("p")
                     .attr("class","homework_mean")
                     .text(function(penguinsDMain){return penguinsDMain.homework_mean}) 
 
-               d3.select("#"+aPenguins.picture.substring(0, aPenguins.picture.length - 4))
+                d3.select("#"+aPenguins.picture.substring(0, aPenguins.picture.length - 4))
                     .append("td")
                     .attr("class","main_table_Cols")
                     .append("p")
                     .attr("class","final_test")
                     .text(function(penguinsDMain){return penguinsDMain.final_test}) 
+                
+                d3.select("#"+aPenguins.picture.substring(0, aPenguins.picture.length - 4))
+                    .append("td")
+                    .attr("class","main_table_Cols")
+                    .style("background-color", aPenguins.final_grade_color)
+                    .append("p")
+                    .attr("class","final_grade")
+                    .text(function(penguinsDMain){return penguinsDMain.final_grade}) 
+                
+
 
             });
         }
             drawIntelforID(penguinsDMain)
         
+        //final_grade Sort Active
+        var final_gradeSortActive = function(penguinsDataMain){
+            d3.select("#L_base_table_final_grade")
+                .on("click",function()
+                    { 
+                        console.log("work")
+                        var penguinsDMean = penguinsDataMain
+                        penguinsDMean.sort(function(a, b){return b.final_grade - a.final_grade})
+                        console.log("work", penguinsDMean)
+                        d3.select("#L_base_table").remove()
+                        drawdata(penguinsDMean)
+                        //drawHeading()
+                        //drawIDS(penguinsDMean)
+                        //drawIntelforID(penguinsDMean)
+                    })
+        }
+        final_gradeSortActive(penguinsDataMain)
         
-        d3.select("#L_base_table_quiz_mean")
-            .on("click",function()
-                { 
-                    console.log("work")
-                    var penguinsDquizMean = penguinsDMain
-                    penguinsDquizMean.sort(function(a, b){return b.quiz_mean - a.quiz_mean})
-                    console.log("work", penguinsDquizMean)
-                    drawhome()
-                    drawdata(penguinsDquizMean)
-                    d3.selectAll(".L_main_table ~ .L_main_table ").remove()
+        //homework_mean Sort Active
+        var homework_meanSortActive = function(penguinsDataMain){
+            d3.select("#L_base_table_homework_mean")
+                .on("click",function()
+                    { 
+                        console.log("work")
+                        var penguinsDMean = penguinsDataMain
+                        penguinsDMean.sort(function(a, b){return b.homework_mean - a.homework_mean})
+                        console.log("work", penguinsDMean)
+                        d3.select("#L_base_table").remove()
+                        drawdata(penguinsDMean)
+                        //drawHeading()
+                        //drawIDS(penguinsDMean)
+                        //drawIntelforID(penguinsDMean)
+                    })
+        }
+        homework_meanSortActive(penguinsDataMain)
+        
+        //final_test Sort Active
+        var final_testSortActive = function(penguinsDataMain){
+            d3.select("#L_base_table_final_test")
+                .on("click",function()
+                    { 
+                        console.log("work")
+                        var penguinsDMean = penguinsDataMain
+                        penguinsDMean.sort(function(a, b){return b.final_test - a.final_test})
+                        console.log("work", penguinsDMean)
+                        d3.select("#L_base_table").remove()
+                        drawdata(penguinsDMean)
+                        //drawHeading()
+                        //drawIDS(penguinsDMean)
+                        //drawIntelforID(penguinsDMean)
+                    })
+        }
+        final_testSortActive(penguinsDataMain)
+        
+        //test_mean Sort Active
+        var test_meanSortActive = function(penguinsDataMain){
+            d3.select("#L_base_table_test_mean")
+                .on("click",function()
+                    { 
+                        console.log("work")
+                        var penguinsDMean = penguinsDataMain
+                        penguinsDMean.sort(function(a, b){return b.test_mean - a.test_mean})
+                        console.log("work", penguinsDMean)
+                        d3.select("#L_base_table").remove()
+                        drawdata(penguinsDMean)
+                        //drawHeading()
+                        //drawIDS(penguinsDMean)
+                        //drawIntelforID(penguinsDMean)
+                    })
+        }
+        test_meanSortActive(penguinsDataMain)
+        
+        //quiz_Mean Sort Active
+        var quizMeanSortActive = function(penguinsDataMain){
+            d3.select("#L_base_table_quiz_mean")
+                .on("click",function()
+                    { 
+                        console.log("work")
+                        var penguinsDMean = penguinsDataMain
+                        penguinsDMean.sort(function(a, b){return b.quiz_mean - a.quiz_mean})
+                        console.log("work", penguinsDMean)
+                        d3.select("#L_base_table").remove()
+                        drawdata(penguinsDMean)
+                        //drawHeading()
+                        //drawIDS(penguinsDMean)
+                        //drawIntelforID(penguinsDMean)
+                    })
+        }
+        quizMeanSortActive(penguinsDataMain)
+        
+        var finalCheck = function(penguinsDataMain) {
             
-                })
+            
+        }
+
         
         /*.on("mouseover",function(aFilmData){
                 d3.selectAll("#info-box *").remove()
